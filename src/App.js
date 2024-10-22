40ed449829ab033a99ec91b6c898a82f92297391
Jing Jing <hxa@HengtekiMacBook-Air.local>
2024-10-21 21:30:11 -0400

    updated navbar signin icon to handle users already signed in to display user account informations & logout button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React from 'react';
-import Navbar from './Navbar'; 
-import Home from './Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './LoginPage'; 
-import SignUpPage from './SignUpPage'
-import PhotographerPage from './PhotographerPage';
-import Privacy from './PrivacyPage';
-import TermsService from './TermsService';
-import Help from './Help';
-import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
+import PhotographerPage from "./PhotographerPage";
+import Privacy from "./PrivacyPage";
+import TermsService from "./TermsService";
+import Help from "./Help";
+import { UserProvider } from "./UserContext";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-    <Routes>
-        <Route path="/" element={<><Navbar /><Home /></>} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />}/>
-        <Route path="/photographer" element={<PhotographerPage />} />
-        <Route path="/privacy-policy" element={<Privacy />} />
-        <Route path="/terms" element={<TermsService />} />
-        <Route path="/help-center" element={<Help />} />
-    </Routes>
-</Router>
+    <UserProvider>
+      {" "}
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Home />
+              </>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/photographer" element={<PhotographerPage />} />
+          <Route path="/privacy-policy" element={<Privacy />} />
+          <Route path="/terms" element={<TermsService />} />
+          <Route path="/help-center" element={<Help />} />
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, browserLocalPersistence, browserSessionPersistence, setPersistence, sendPasswordResetEmail } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  browserLocalPersistence,
+  browserSessionPersistence,
+  setPersistence,
+  sendPasswordResetEmail,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -7,13 +17,23 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const googleProvider = new GoogleAuthProvider();
 
-export { auth, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, googleProvider, setPersistence, browserLocalPersistence, browserSessionPersistence, sendPasswordResetEmail };
+export {
+  app,
+  auth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  googleProvider,
+  setPersistence,
+  browserLocalPersistence,
+  browserSessionPersistence,
+  sendPasswordResetEmail,
+};
